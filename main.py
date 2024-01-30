28c1059deda5e40bc951070a679a631702066ec5
Dawson Dittmar <dawson.dittmar@gmail.com>
2024-01-30 11:07:13 -0600

    made enemies and stuff

--- main.py
+++ main.py
@@ -1,15 +1,45 @@
 import pygame
 import sys
+import random
 
 # Classes
 class Player(pygame.sprite.Sprite):
     def __init__(self):
         super(Player,self).__init__()
         self.color = "Blue"
+        self.files = "player_car.png"
+        self.index = 0
+        self.images = pygame.image.load("player_car.png").convert_alpha()
+        self.image = self.images
+        self.rect = self.image.get_rect(center = (400,580))
+
+    def collide(self):
+        hit = pygame.sprite.spritecollide(self,enemies, False)
+        if hit:
+            self.kill()
+
 
 class Car(pygame.sprite.Sprite):
-    def __init__(self):
+    def __init__(self, x, y):
         super(Car,self).__init__()
+        self.index = random.randint(0,2)
+        self.files = ["enemy_car1.png","enemy_car2.png","enemy_car3.png"]
+        self.images = [pygame.image.load(filename).convert_alpha() for filename in self.files]
+        self.image = self.images[self.index]
+        self.rect = self.image.get_rect(center = (x,y))
+
+    def move(self):
+        self.rect.centery += 7
+        if self.rect.centery >= 800:
+            self.kill()
+
+class Background(pygame.sprite.Sprite):
+    def __init__(self, x, y):
+        super(Background, self).__init__()
+        self.files = ["enemy_car1.png","enemy_car2.png","enemy_car3.png"]
+        self.images = [pygame.image.load(filename).convert_alpha() for filename in self.files]
+        self.image = self.images[self.index]
+        self.rect = self.image.get_rect(center = (x,y))
 
 # Initialize Pygame and give access to all the methods in the package
 pygame.init()
@@ -35,6 +65,16 @@ grass.fill("#09B009")
 
 player = Player()
 
+enemies = pygame.sprite.Group()
+enemies.add(Car(random.choice([210,230,250,270,290,310,330,350,370,390,410,430,450,470,490,510,530,550,570,590]),0))
+
+objects = pygame.sprite.Group()
+objects.add(player)
+
+font = pygame.font.Font(None,100)
+text_surface = font.render("Game Over", False,"Red")
+
+
 # Main game loop
 running = True
 while running:
@@ -50,11 +90,29 @@ while running:
     screen.blit(grass,(0,0))
     screen.blit(grass,(600,0))
 
+    if random.randint(1,10) == 1:
+        enemies.add(Car(random.randint(210,590),0))
+        objects.add(enemies)
+
+
+    for car in enemies:
+        car.move()
+
+    player.collide()
+
+    objects.draw(screen)
+
+    if pygame.sprite.Sprite.alive(player) == False:
+        screen.blit(text_surface,(200,250))
+
+
     # Update the display
     pygame.display.flip()
 
     # Set a frame rate to 60 frames per second
     clock.tick(60)
+
+
 
 # Quit Pygame properly
 pygame.quit()
